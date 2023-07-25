7faff398481bd653d13587fef78518daf159c959
Batuhan ÖZALP <batuhan.ozalp.9@yandex.com>
2023-07-25 14:02:10 +0300

    .

--- script.js
+++ script.js
@@ -6,7 +6,6 @@ for (let i = 0; i < 20; i++) {
 
     for (let j = 0; j < 20; j++) {
         const cell = document.createElement("td");
-       // cell.textContent = `${i + 1},${j + 1}`; // Hücre içine sırasını yazabilirsiniz, isteğe bağlı.
         row.appendChild(cell);
         //cell.style.backgroundColor="gray";
     }
@@ -19,23 +18,3 @@ buttons.forEach(b => {
     b.style.backgroundColor="red"
     );
 })
-
-
-
-//bunu fonksyiona çevir bitince
-/*let x, y = 0;
-for (let i = 1; i <= 100; i++) {
-    const newBox = document.createElement("div");
-    newBox.className = "empty-box";
-
-    newBox.style.left = x + 'px';
-    if (i > )
-        x += 20.25;
-    newBox.style.top = y + 'px';
-    if (i % 10 === 1) {
-        y += 20.25;
-        x = 0;
-    }
-    canva.appendChild(newBox);
-    newBox.style.position = "absolute";
-}*/
